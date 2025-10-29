ac58ada43473e5f78620310bbfbc4291c8bc7700
Khadim Gaye <gaye93300k@gmail.com>
2025-10-29 04:08:18 +0100

    Suppresion des fichiers inutils

--- inventaire.py
+++ inventaire.py
@@ -1,11 +1,22 @@
 class Inventaire:
-    def __init__(self):
-        self.pas = 70
-        self.gemmes = 2
-        self.cles = 0
-        self.des = 0
-        self.objets_permanents = set()
+    def __init__(self, pas=70, or_=0, gemmes=2, cles=0, des=0, permanents=None):
+        self.pas = pas
+        self.or_ = or_
+        self.gemmes = gemmes
+        self.cles = cles
+        self.des = des
+        self.permanents = permanents if permanents else set()
 
+<<<<<<< HEAD
+    def depenser_pas(self, n=1):
+        if self.pas >= n:
+            self.pas -= n
+            return True
+        return False
+
+    def afficher(self):
+        return f"Pas: {self.pas} | Or: {self.or_} | Gemmes: {self.gemmes} | Clés: {self.cles} | Dés: {self.des}"
+=======
     def ajouter_objet(self, objet):
         if objet.type == "permanent":
             self.objets_permanents.add(objet.nom)
@@ -14,4 +25,4 @@ class Inventaire:
         elif objet.nom == "pas":
             self.pas += objet.valeur
 
-print("maysa baye")
+>>>>>>> 7c2f297b90d267df6b41ae4bfe755ca221c13b63
