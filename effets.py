9ecff2723e6e8d34608c7c23d740812afe4cd4a4
Khadim Gaye <gaye93300k@gmail.com>
2025-10-31 01:19:04 +0100

    le manoir plus complexe et des effets ajoutes

--- effets.py
+++ effets.py
@@ -29,13 +29,71 @@ def _effet_observatoire(piece: Piece, ctx: Contexte) -> str: # Exple observatoir
     ctx.inventaire.ajouter_cles(1)
     return "Observatoire : +1 clé"
 
+def _effet_pomme(piece: Piece, ctx: Contexte) -> str:
+    ctx.inventaire.ajouter_pas(2)
+    return "Pomme : +2 pas"
+
+def _effet_banane(piece: Piece, ctx: Contexte) -> str:
+    ctx.inventaire.ajouter_pas(3)
+    return "Banane : +3 pas"
+
+def _effet_sandwich(piece: Piece, ctx: Contexte) -> str:
+    ctx.inventaire.ajouter_pas(10)
+    return "Sandwich : +10 pas"
+
+def _effet_salade(piece: Piece, ctx: Contexte) -> str:
+    ctx.inventaire.ajouter_pas(5)
+    return "Salade : +5 pas"
+
+def _effet_repas(piece: Piece, ctx: Contexte) -> str:
+    ctx.inventaire.ajouter_pas(25)
+    return "Repas : +25 pas"
+
+def _effet_coffre(piece: Piece, ctx: Contexte) -> str:
+    if ctx.inventaire.depenser_cles(1):
+        ctx.inventaire.ajouter_gemmes(1)
+        ctx.inventaire.ajouter_pas(5)
+        return "Coffre ouvert : +1 gemme, +5 pas"
+    return "Coffre fermé : clé manquante"
+
+def _effet_endroitacreuser(piece: Piece, ctx: Contexte) -> str:
+    if "pelle" in ctx.inventaire.permanents:
+        ctx.inventaire.ajouter_pas(3)
+        return "Creusé : +3 pas"
+    return "Impossible de creuser : pelle manquante"
+
+def _effet_casier(piece: Piece, ctx: Contexte) -> str:
+    if ctx.inventaire.depenser_cles(1):
+        ctx.inventaire.ajouter_pas(4)
+        return "Casier ouvert : +4 pas"
+    return "Casier fermé : clé manquante"
 
 # mapping  (clé = nom normalisé en minuscules)
+# EFFETS_PAR_TYPE = {
+#     "serre": _effet_serre,
+#     "fournaise": _effet_fournaise,
+#     "observatoire": _effet_observatoire,
+# }
+def _effet_casier(piece: Piece, ctx: Contexte) -> str:
+    if ctx.inventaire.depenser_cles(1):
+        ctx.inventaire.ajouter_pas(4)
+        return "Casier ouvert avec clé : +4 pas"
+    return "Casier fermé : clé manquante"
+
 EFFETS_PAR_TYPE = {
     "serre": _effet_serre,
     "fournaise": _effet_fournaise,
     "observatoire": _effet_observatoire,
+    "pomme": _effet_pomme,
+    "banane": _effet_banane,
+    "sandwich": _effet_sandwich,
+    "salade": _effet_salade,
+    "repas": _effet_repas,
+    "coffre": _effet_coffre,
+    "endroitacreuser": _effet_endroitacreuser,
+    "casier": _effet_casier,
 }
+
 
 
 def _norm(n: str) -> str:
